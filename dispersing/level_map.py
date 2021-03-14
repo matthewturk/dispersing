633913af7bdaffc5bfe00a9f625fcc4d381dd608
Matthew Turk <matthewturk@gmail.com>
2021-03-14 14:47:44 -0500

    Start refactoring levels

--- dispersing/level_map.py
+++ dispersing/level_map.py
@@ -1,11 +1,52 @@
 import numpy as np
+import ipywidgets
+from IPython.display import display
+
+_terrain_attrs = (
+    "internal_wall_edges",
+    "floor",
+    "floor_special_tile",
+    "wall_edges",
+    "keys_switches",
+    "door",
+    "unk7",
+    "unk8",
+    "unk9",
+    "unk12",
+    "big_wooden_thing",
+    "big_boulder",
+)
+
+
+class TerrainSprites(dict):
+    def __init__(self, game, level_asset):
+        super(dict, self).__init__()
+        self.game = game
+        self.level_asset = level_asset
+        offset = self.game.assets["INIT"].sprite_offsets.terrain
+        sprites = self.game.resources.sprites
+        props = self.level_asset.properties
+
+        for attr in _terrain_attrs:
+            self[attr] = sprites[offset + getattr(props, attr)]
+        offset = self.game.assets["INIT"].sprite_offsets.wall_decoration
+        self["wall_overlay_tiles"] = sprites[offset + props.wall_overlay_tiles]
+
+    def _ipython_display_(self):
+        children = []
+        for attr in _terrain_attrs + ("wall_overlay_tiles",):
+            o = ipywidgets.Output()
+            with o:
+                display(self[attr])
+            children.append(ipywidgets.HBox([ipywidgets.Label(attr), o]))
+        display(ipywidgets.VBox(children))
 
 
 class LevelMap:
     def __init__(self, game, level_number):
         self.game = game
         self.level_asset = game.assets["LEVELS"].levels[level_number]
-        self.level_tiles = np.frombuffer(self.level_asset.map, dtype="u1").reshape(
+        self.tiles = np.frombuffer(self.level_asset.map, dtype="u1").reshape(
             (self.level_asset.height, self.level_asset.width)
         )
 
@@ -16,5 +57,5 @@ class LevelMap:
             self.items[item.x, item.y] = [game.objects[_] for _ in item.info.items]
             self.info[item.x, item.y] = item.info
 
-    def __getitem__(self, item):
-        return self.items.get(item, []), self.info.get(item, None)
+        # Setup terrain sprites
+        self.terrain_sprites = TerrainSprites(self.game, self.level_asset)
--- dispersing/sprite_resource.py
+++ dispersing/sprite_resource.py
@@ -1,6 +1,9 @@
-import numpy as np
 from .fast_utilities import unpack_sprite_algo3
+import numpy as np
 import PIL.Image
+import io
+import ipywidgets
+from IPython.display import display
 
 
 class SpriteResource:
@@ -23,3 +26,28 @@ class SpriteResource:
         palette = np.concatenate([palettes[pal1_id], palettes[pal2_id]], axis=0)
         for frame in range(im.shape[-1]):
             self.frames.append(PIL.Image.fromarray(palette[im[:, :, frame]]))
+
+    def _ipython_display_(self):
+        png_images = []
+        for fr in self.frames:
+            with io.BytesIO() as output:
+                fr.save(output, format="PNG")
+                output.seek(0)
+                png_images.append(output.read())
+        frame_slider = ipywidgets.IntSlider(min=0, max=len(png_images) - 1, step=1)
+        im = ipywidgets.Image(value=b"", format="png", height=200)
+        html = (
+            "<style>.dispersing-pixelated {image-rendering: pixelated;}</style><table>"
+        )
+        display(ipywidgets.HTML(html))
+
+        def update_image(change):
+            im.value = png_images[frame_slider.value]
+
+        im.add_class("dispersing-pixelated")
+        im.layout.height = "200px"
+        im.layout.object_fit = "contain"
+        frame_slider.observe(update_image, "value")
+        update_image(None)
+        # There's gotta be a better way to do this, but.
+        display(ipywidgets.VBox([im, frame_slider]))
